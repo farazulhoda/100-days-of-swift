bfcd1cd9d14d649a3ad5e16e685963416e6e8ee2
farazul <firaz5973616@gmail.com>
2020-12-06 03:09:23 +0530

    xcodeloop.playground

--- src/loops.playground/Contents.swift
+++ src/loops.playground/Contents.swift
@@ -8,3 +8,15 @@ for number in count {
     print("Number is \(number)")
 }
 
+let albums = ["Red", "1997", "Reputation"]
+
+for album in albums {
+    print("\(album) is on Apple music")
+}
+
+print("Players gonna ")
+
+for _ in 1...5 {
+    print("play")
+}
+
