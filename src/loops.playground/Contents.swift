10a63f703e4ed0ffd0b5851bd8768cedaa6bfc9d
farazul <firaz5973616@gmail.com>
2020-12-08 02:14:22 +0530

    loopsxcode.playground

--- src/loops.playground/Contents.swift
+++ src/loops.playground/Contents.swift
@@ -61,4 +61,24 @@ while countDown >= 0 {
     countDown -= 1
 }
 
+//Exiting multiple loops
+for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print("\(i) * \(j) is \(product)")
+        
+    }
+}
 
+//break outerLoop
+outerLoop: for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print("\(i) * \(j) is \(product)")
+        
+        if product == 50 {
+            print("It's a Bullseye!")
+            break outerLoop
+        }
+    }
+}
