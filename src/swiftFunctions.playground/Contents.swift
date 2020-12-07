79765694083eafef56b2b29dd55bc60fcc8849ca
farazul <firaz5973616@gmail.com>
2020-12-08 03:06:29 +0530

    swiftFunctions.playground

---
+++ src/swiftFunctions.playground/Contents.swift
@@ -0,0 +1,17 @@
+// https://dev.to/farazul
+// Farazul Hoda - @farazul.hoda
+import UIKit
+
+//writing functions
+func printHelp()
+{
+    let message = """
+welcome to myApp!
+
+Run this app inside a dir of images and
+myApps will resize them all into thumbnails
+"""
+    print(message)
+}
+//we can now run that using printHelp()
+printHelp()
