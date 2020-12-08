5cfdc93c6e11132f3ac8f1355aece480ce4dbaa4
farazul <firaz5973616@gmail.com>
2020-12-08 21:06:42 +0530

    swiftFunc.playground

--- src/swiftFunctions.playground/Contents.swift
+++ src/swiftFunctions.playground/Contents.swift
@@ -15,3 +15,18 @@ myApps will resize them all into thumbnails
 }
 //we can now run that using printHelp()
 printHelp()
+
+//Accepting parameters
+print("Hello")                                                        //no sending of stringsInt
+
+func square(number: Int) {
+    print(number * number)
+}
+square(number: 8)                                                    //accepting parameters
+
+//returning values
+func sq(num: Int) -> Int {
+    return num * num
+}
+let rslt = sq(num: 2)
+print(rslt)
