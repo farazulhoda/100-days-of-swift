498c579627b36103c6db23404109cf335b345d3b
farazul <firaz5973616@gmail.com>
2020-12-10 20:40:39 +0530

    returning-values

--- src/closures-one.playground/Contents.swift
+++ src/closures-one.playground/Contents.swift
@@ -6,3 +6,23 @@ let driving = {
     print("I'm Driving my car")
 }
 driving()
+
+//accepting parameters
+let driving = { (place: String) in
+    print("I'm going place to \(place) in my car")
+}
+
+driving("Chennai")
+
+//Returning values
+let driving = { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+let drivingWithReturn = { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+let mesaage = drivingWithReturn("Chennai")
+print(mesaage)
+
+
+
