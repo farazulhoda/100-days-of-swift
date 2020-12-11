2eca162a3d53faf23c46b79231cfb551d546cd5d
farazul <firaz5973616@gmail.com>
2020-12-11 23:58:47 +0530

    closure-parameters

--- src/closures-one.playground/Contents.swift
+++ src/closures-one.playground/Contents.swift
@@ -2,17 +2,17 @@
 // Farazul Hoda - @farazul.hoda
 import UIKit
 // creating basic closures
-let driving = {
+let drivngg = {
     print("I'm Driving my car")
 }
-driving()
+drivngg()
 
 //accepting parameters
-let driving = { (place: String) in
+let drivingg = { (place: String) in
     print("I'm going place to \(place) in my car")
 }
 
-driving("Chennai")
+drivingg("Chennai")
 
 //Returning values
 let driving = { (place: String) in
@@ -24,5 +24,14 @@ let drivingWithReturn = { (place: String) -> String in
 let mesaage = drivingWithReturn("Chennai")
 print(mesaage)
 
-
+//closures as prameters
+let drivin = {
+    print("I'm driving in my car")
+}
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+travel(action: drivin)
 
