2a735cafd6d6cb52a9e1a74442359dfbc21107f4
farazul <firaz5973616@gmail.com>
2020-12-22 01:32:26 +0530

    creating-structs.playground

---
+++ src/structs.playground/Contents.swift
@@ -0,0 +1,13 @@
+// https://dev.to/farazul
+// Farazul Hoda
+import UIKit
+
+//creating structs
+struct Sport    {
+    var name: String
+    
+}
+
+var cricket = Sport(name: "Cricket")
+print(cricket.name)
+cricket.name = "Lord's Cricket Ground"
