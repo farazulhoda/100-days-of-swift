57af16d7c11b14db2abbeff02d5de05b6fc0a06a
farazul <firaz5973616@gmail.com>
2020-12-22 01:34:19 +0530

    Added Comments

--- src/structs.playground/Contents.swift
+++ src/structs.playground/Contents.swift
@@ -5,9 +5,9 @@ import UIKit
 //creating structs
 struct Sport    {
     var name: String
-    
 }
-
 var cricket = Sport(name: "Cricket")
 print(cricket.name)
+// "name" and "cricket" variable, so we can change
+//them just like regular variables.
 cricket.name = "Lord's Cricket Ground"
