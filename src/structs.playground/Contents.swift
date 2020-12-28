d60f51734c7c1c5110886239b75a35a9044632ce
farazul <firaz5973616@gmail.com>
2020-12-29 00:09:00 +0530

    computed-properties

--- src/structs.playground/Contents.swift
+++ src/structs.playground/Contents.swift
@@ -11,3 +11,20 @@ print(cricket.name)
 // "name" and "cricket" variable, so we can change
 //them just like regular variables.
 cricket.name = "Lord's Cricket Ground"
+
+//computedProperties
+struct Sports   {
+var game: String
+var isOlympicSport: Bool
+
+    var olympicStatus: String  {
+    if isOlympicSport {
+        return "\(game) is an Olympic sport"
+    }   else    {
+        return "\(game) is NOT an Olympic sport"
+    }
+}
+}
+
+let chessBoxing = Sports(game: "Boxing", isOlympicSport: false)
+print(chessBoxing.olympicStatus)
